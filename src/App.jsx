e500d94d3377a4bc218461ce8c10622e1b832e0d
Hunter-137 <asrarkulovamal@gmail.com>
2025-01-22 15:01:43 +0500

    feat: add backend with mockapi, fetch pizzas data from mockapi

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,28 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
-import pizzas from "./assets/pizzas.json";
 
 import "./scss/app.scss";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          "https://6790ae6caf8442fd73773b6f.mockapi.io/items"
+        );
+        const result = await response.json();
+        setItems(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -18,7 +35,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => {
+            {items.map((obj) => {
               return <PizzaBlock key={obj.title} {...obj} />;
             })}
           </div>
